0464c175c2d6b95c692647b031a9ac62da435309
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-07-21 17:21:25 -0300

    update project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppImportsModule } from './shared/app-imports/app-imports.module';
 import { HttpClientModule } from '@angular/common/http';
+import { FooterComponent } from './swiftcash/containers/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
