3add8dd63ca338dd819c8ad596173213a43fc7ca
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-07-20 21:04:22 -0300

    update app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './swiftcash/containers/home/home.component';
+import { LoginComponent } from './swiftcash/containers/login/login.component';
 
 
 const routes: Routes = [
   { path:'', pathMatch:'full', redirectTo:'home' },
-  { path:'home',
-  loadChildren: () => import('./swiftcash/swiftcash.module').then(m => m.SwiftCashModule) }
+  { path:'home', component: HomeComponent },
+  { path:'login', component: LoginComponent },
+
+  { path: 'swiftcash', loadChildren: () => import('./swiftcash/swiftcash.module').then(m => m.SwiftCashModule) }
 ];
 
 @NgModule({
