ee999be45954a7e8182c3d388c1f137b1c2b3f16
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-07-23 19:37:52 -0300

    update app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,19 +2,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './swiftcash/containers/home/home.component';
 import { LoginComponent } from './swiftcash/containers/login/login.component';
-
+import { TaxComponent } from './swiftcash/containers/tax/tax.component';
+import { SolutionComponent } from './swiftcash/containers/solution/solution.component';
+import { HelpComponent } from './swiftcash/containers/help/help.component';
 
 const routes: Routes = [
-  { path:'', pathMatch:'full', redirectTo:'home' },
-  { path:'home', component: HomeComponent },
-  { path:'login', component: LoginComponent },
-
-  { path: 'swiftcash', loadChildren: () => import('./swiftcash/swiftcash.module').then(m => m.SwiftCashModule) }
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'tax', component: TaxComponent },
+  { path: 'solution', component: SolutionComponent },
+  { path: 'help', component: HelpComponent },
+  { path: 'swiftcash', loadChildren: () => import('./swiftcash/swiftcash.module').then(m => m.SwiftCashModule) },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-
 export class AppRoutingModule { }
