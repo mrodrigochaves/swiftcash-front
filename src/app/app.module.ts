b8088bf2b176a53caf1b1f380c9c6ec45c5d1415
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-07-24 20:58:54 -0300

    update app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppImportsModule } from './shared/app-imports/app-imports.module';
 import { HttpClientModule } from '@angular/common/http';
 import { FooterComponent } from './swiftcash/containers/footer/footer.component';
+import { HeaderComponent } from './swiftcash/containers/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
     FooterComponent,
   ],
   imports: [
