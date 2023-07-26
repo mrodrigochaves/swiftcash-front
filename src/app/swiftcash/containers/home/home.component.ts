a0c1a61c6964d968e791dd34a5eea96256921275
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-07-26 20:45:25 -0300

    update home repo

--- src/app/swiftcash/containers/home/home.component.ts
+++ src/app/swiftcash/containers/home/home.component.ts
@@ -1,18 +1,11 @@
 import { Component } from '@angular/core';
-import { CarouselConfig } from 'ngx-bootstrap/carousel';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  providers: [
-    { provide: CarouselConfig, useValue: { interval: 1500, noPause: false, showIndicators: true } }
- ],
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-  slides = [
-    { image: '../../../../assets/cash.jpg', text: 'cash image' },
-    { image: '../../../../assets/card.jpg', text: 'card image' },
 
-  ];
 }
