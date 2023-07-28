a331989b3bd4d2b2336c9a0afd73a2e315fdeca9
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-07-28 08:48:08 -0300

    update app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,24 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
-import { AppImportsModule } from './shared/app-imports/app-imports.module';
+import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { FooterComponent } from './swiftcash/components/footer/footer.component';
-import { HeaderModule } from './swiftcash/components/header/header.module';
-import { CarouselModule } from '../app/swiftcash/components/carousel/carousel.module'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    FooterComponent,
+    declarations: [
+        AppComponent,
+    ],
+    imports: [
+      BrowserModule,
+      BrowserAnimationsModule,
+      AppRoutingModule,
+      HttpClientModule,
+
   ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    AppRoutingModule,
-    HttpClientModule,
-    AppImportsModule,
-    HeaderModule,
-    CarouselModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    providers: [],
+    bootstrap: [AppComponent],
+
 })
 export class AppModule { }
